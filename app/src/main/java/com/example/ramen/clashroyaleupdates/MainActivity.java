e847252ca2cdb2e920e7c6596d4f858c61412e0a
Rrishik <ramenarishik@gmail.com>
2017-01-03 19:44:56 +0530

    init

---
+++ app/src/main/java/com/example/ramen/clashroyaleupdates/MainActivity.java
@@ -0,0 +1,69 @@
+package com.example.ramen.clashroyaleupdates;
+
+import android.content.Context;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        loadData();
+    }
+
+    public void loadData(){
+        final TextView mTextView = (TextView) findViewById(R.id.tv_main);
+
+        // Instantiate the RequestQueue.
+        RequestQueue queue = Volley.newRequestQueue(this);
+        String url ="https://clashroyale.com/blog/news";
+        // Request a string response from the provided URL.
+        StringRequest stringRequest = new StringRequest(Request.Method.GET, url,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        // Display the first 500 characters of the response string.
+                        mTextView.setText(response);
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                mTextView.setText("That didn't work!");
+            }
+        });
+        // Add the request to the RequestQueue.
+        queue.add(stringRequest);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main,menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int itemClicked = item.getItemId();
+        if (itemClicked == R.id.action_refresh){
+            Context context = MainActivity.this;
+            String toastText = "Refresh";
+            Toast.makeText(context,toastText,Toast.LENGTH_SHORT).show();
+            loadData();
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}
