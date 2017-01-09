858add34a661ff06eb882f1a7cdb2e2ae4af4b4b
Rrishik <ramenarishik@gmail.com>
2017-01-09 21:11:28 +0530

    dates added
    string arrays converted to an object array

---
+++ app/src/main/java/com/example/ramen/clashroyaleupdates/adapter/AdapterData.java
@@ -0,0 +1,19 @@
+package com.example.ramen.clashroyaleupdates.adapter;
+
+
+/**
+ * Created by ramen on 09-01-2017.
+ */
+
+public class AdapterData {
+    public String mNewsData;
+    public String mImageLink;
+    public String mDate;
+
+    public AdapterData(String mNewsData,String mImageLink, String mDate){
+        this.mNewsData = mNewsData;
+        this.mImageLink = mImageLink;
+        this.mDate = mDate;
+    }
+
+}
